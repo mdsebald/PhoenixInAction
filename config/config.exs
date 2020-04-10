53b6d5b0336baded740f161142b33e2d533b8d4f
mdsebald <mdsebald@gmail.com>
2020-04-10 06:01:03 -0500

    Phoenix In Action, Chap 11

---
+++ config/config.exs
@@ -0,0 +1,53 @@
+# This file is responsible for configuring your umbrella
+# and **all applications** and their dependencies with the
+# help of the Config module.
+#
+# Note that all applications in your umbrella share the
+# same configuration and dependencies, which is why they
+# all use the same configuration file. If you want different
+# configurations or dependencies per app, it is best to
+# move said applications out of the umbrella.
+import Config
+
+config :auction_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :auction_web, AuctionWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "2rtJMKlwSk620uuxwp7uqeFaMWKm06wo5LNuJqUfxIbjaf/KJOkgT1uqmeWULKGv",
+  render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "BMKjQpZw"]
+
+# Sample configuration:
+#
+#     config :logger, :console,
+#       level: :info,
+#       format: "$date $time [$level] $metadata$message\n",
+#       metadata: [:user_id]
+#
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+# What repo(s) are we using
+config :auction, ecto_repos: [Auction.Repo]
+
+# Configure the repo
+# Can use dev.exs, prod.exs, etc to vary by environment
+config :auction, Auction.Repo,
+  database: "auction",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: 5432
