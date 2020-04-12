68ba0d7dbb76035775460835363ea07a22ac57ec
mdsebald <mdsebald@gmail.com>
2020-04-12 15:45:19 -0500

    Chapter 13

---
+++ apps/auction_web/lib/auction_web/views/api/user_view.ex
@@ -0,0 +1,12 @@
+defmodule AuctionWeb.Api.UserView do
+  use AuctionWeb, :view
+
+  def render("user.json", %{user: user}) do
+    %{
+      type: "user",
+      id: user.id,
+      username: user.username
+    }
+  end
+
+end
